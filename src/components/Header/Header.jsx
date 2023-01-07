355fffd1ce8ac7b948b364ad30afaf0ac1d07feb
motochowdhury <motochowdhury@gmail.com>
2023-01-07 20:39:03 +0600

    nav logo and item added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,48 @@ import "./Header.css";
 export const Header = () => {
   return (
     <>
-      <header></header>
+      <header className="header">
+        <nav className="nav container">
+          <a href="index.html" className="lav_logo">
+            MotoChowdhury
+          </a>
+          <div className="nav_menu">
+            <ul className="nav_list grid">
+              <li className="nav_item">
+                <a href="#home" className="nav_link">
+                  <i className="uil uil-estate nav_icon"></i>
+                  Home
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#about" className="nav_link">
+                  <i className="uil uil-user nav_icon"></i>
+                  About
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#skills" className="nav_link">
+                  <i className="uil uil-file-alt nav_icon"></i>
+                  Skills
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#services" className="nav_link">
+                  <i className="uil uil-briefcase-alt nav_icon"></i>
+                  Services
+                </a>
+              </li>
+              <li className="nav_item">
+                <a href="#contact" className="nav_link">
+                  <i className="uil uil-briefcase-alt nav_icon"></i>
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <i className="uil uil-times nav_close"></i>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
