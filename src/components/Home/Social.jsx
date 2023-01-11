a0b513038c3550527d06841c6d1ba00bb18918f6
motochowdhury <motochowdhury@gmail.com>
2023-01-11 22:40:37 +0600

    Social Link added and add hover effect in social button

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -3,16 +3,36 @@ import React from "react";
 export const Social = () => {
   return (
     <div className="home_social">
-      <a href="" className="home_social-icon" target="_blank">
+      <a
+        href="https://www.instagram.com/motochowdhury/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i class="uil uil-instagram"></i>
       </a>
-      <a href="" className="home_social-icon" target="_blank">
+      <a
+        href="https://github.com/motochowdhury"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i class="uil uil-github-alt"></i>
       </a>
-      <a href="" className="home_social-icon" target="_blank">
+      <a
+        href="https://api.whatsapp.com/send?phone=01870463513"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i class="uil uil-whatsapp"></i>
       </a>
-      <a href="" className="home_social-icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/motoc/"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i class="uil uil-linkedin-alt"></i>
       </a>
     </div>
